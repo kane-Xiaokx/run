fd3847de83d93bb6200feb59e75a898eacf135ed
koch-Xiaokx <545593230@qq.com>
2022-03-15 19:01:17 +0800

    commit

---
+++ rocketMQ/src/test/java/cn/koch/rocket/RocketMqApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.koch.rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
